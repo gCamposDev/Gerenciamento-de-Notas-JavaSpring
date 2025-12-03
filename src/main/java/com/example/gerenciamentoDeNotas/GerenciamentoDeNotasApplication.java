0875b8a96ccaca8033e509809815b88947f72ba5
Gabriel Campos <gcamposlive@gmail.com>
2025-12-03 13:05:10 -0300

    Project Spring

---
+++ src/main/java/com/example/gerenciamentoDeNotas/GerenciamentoDeNotasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamentoDeNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamentoDeNotas/GerenciamentoDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamentoDeNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
