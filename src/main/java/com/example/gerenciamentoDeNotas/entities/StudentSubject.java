1c4c73f5a4ae9c94ed3d9d803ccc39eac8eb3eeb
Gabriel Campos <gcamposlive@gmail.com>
2025-12-28 12:26:23 -0300

    uniqueConstraints

--- src/main/java/com/example/gerenciamentoDeNotas/entities/StudentSubject.java
+++ src/main/java/com/example/gerenciamentoDeNotas/entities/StudentSubject.java
@@ -5,35 +5,71 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table
+@Table(
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"student_id", "subject_id"})}
+)// Garante que não existam registros duplicados para o mesmo student e subject,
+// ou seja, não é possível registrar duas studentPerformance do mesmo aluno e da mesma matéria
 public class StudentSubject {
 
-    @EmbeddedId
-    private StudentSubjectPK id = new StudentSubjectPK();
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Double grade;
     private Integer absence;
 
-    public StudentSubject(){
+    @ManyToOne
+    @JoinColumn(name= "student_id")
+    private Student student;
 
-    }
+    @ManyToOne
+    @JoinColumn(name="subject_id")
+    private Subject subject;
 
-    public StudentSubject(Student student, Subject subject, Double grade, Integer absence) {
-        this.id.setStudent(student);
-        this.id.setSubject(subject);
+    public StudentSubject(Student student, Subject subject, Double grade, Integer absence ) {
+        this.student = student;
+        this.subject = subject;
         this.grade = grade;
         this.absence = absence;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        StudentSubject that = (StudentSubject) o;
-        return Objects.equals(id, that.id);
+    public Double getGrade() {
+        return grade;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Integer getAbsence() {
+        return absence;
+    }
+
+    public void setAbsence(Integer absence) {
+        this.absence = absence;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
